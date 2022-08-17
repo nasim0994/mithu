af9c683e61c39b4ef97ed4a7d9d12423f2599686
nasim0994 <nasimuddin0994@gmail.com>
2022-08-17 10:58:36 +0600

    last commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-btn").addEventListener("click", function () {
+  document.getElementById("exampleFormControlInput1").value = "";
+  document.getElementById("exampleFormControlInput2").value = "";
+  document.getElementById("exampleFormControlTextarea1").value = "";
+});
